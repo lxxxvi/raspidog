722b61b1341ddf4b0440e08a01d98bad4e473459
Mario Schüttel <mario@lxxxvi.ch>
2016-07-02 12:41:50 +0000

    serving multiple clients and multiple hosts

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var sh = require('shelljs');
 sh.config.silent = true;
 require('shelljs/global');
 
+
+// ROUTES
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -18,57 +21,85 @@ app.get('/stylesheet', function(req, res) {
   res.sendFile(__dirname + '/css/raspifarm-dog.css')
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
 
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-    clearInterval(time);
-  });
+// CONNECTIONS
 
+var guests = 0;
 
-  time=setInterval(function(){
-    exec(__dirname + '/workload.sh 192.168.17.15', function(status, output) {
-      var workload = output.split(',');
-      var cpu_idle          = workload[0];
-      var memory_available  = workload[1] - 0;   // 745392
-      var memory_total      = workload[2] - 0;   // 948012
-      var memory_shared     = workload[3] - 0;   // 12260
-      var memory_buffer     = workload[4] - 0;   // 16728
-      var memory_cached     = workload[5] - 0;   // 147876
+io.on('connection', function(socket) {
 
-      // console.log(cpu_idle,memory_available,memory_total,memory_shared,memory_buffer,memory_cached);
-      var memory_free = memory_available + memory_shared + memory_buffer + memory_cached; // 922256
-      // console.log("free", memory_free);
-      var memory_used = (memory_total - memory_free ) ;
+    if(guests === 0) {
+      time = setInterval(function() { deliverWorkloads(socket) }, 1000);
+      console.log("interval started");
+    }
 
-      var response = {
-        "cpu": { 
-          "used_percent": (100 - cpu_idle)
-        },
-        "memory": {
-          "used_percent": Math.round(( memory_used * 100 ) / memory_total),
-          "used"        : memory_used,
-          "total"       : memory_total
-       }
-      };
-
-      io.emit('workload', response);
+    socket.on('disconnect', function() {
+      guests -= 1;
+      console.log('guest disconnected');
+      if(guests === 0) {
+        clearInterval(time);
+        console.log("interval cleared");
+      }
     });
-    // exec('date', function(status, output) {
-    //  io.emit('get date', "bla: " + output);
-    // });
-  },1000);
 
-  socket.on('get date', function() {
-    console.log("request received");
+    guests += 1;
+    console.log('guest connected (' + guests + ' guests)');
+}); 
 
-    // exec('date', function(status, output) {
-    //  io.emit('get date', output);
-    // });
+
+// deliver workloads for hosts
+
+function deliverWorkloads(socket) {
+  [
+    '192.168.17.15',
+    '192.168.17.16',
+    '192.168.17.17',
+    '192.168.17.18',
+  ].map(function(address) {
+    workloadFor(address, socket);
   });
+}
 
-});
+function workloadFor(ipAddress, socket) {
+
+    exec(__dirname + '/workload.sh ' + ipAddress, function(status, output) {
+
+      if(status === 0) {
+        var workload = output.split(',');
+        var cpu_idle          = workload[0];
+        var memory_available  = workload[1] - 0;   // 745392
+        var memory_total      = workload[2] - 0;   // 948012
+        var memory_shared     = workload[3] - 0;   // 12260
+        var memory_buffer     = workload[4] - 0;   // 16728
+        var memory_cached     = workload[5] - 0;   // 147876
+
+        // console.log(cpu_idle,memory_available,memory_total,memory_shared,memory_buffer,memory_cached);
+        var memory_free = memory_available + memory_shared + memory_buffer + memory_cached; // 922256
+        // console.log("free", memory_free);
+        var memory_used = (memory_total - memory_free ) ;
+
+        var response = {
+          "ip_address": ipAddress,
+          "workload": {
+            "cpu": { 
+                "used_percent": (100 - cpu_idle)
+              },
+              "memory": {
+                "used_percent": Math.round(( memory_used * 100 ) / memory_total),
+                "used"        : memory_used,
+                "total"       : memory_total
+             }  
+          }
+        };
+
+        socket.broadcast.emit('workload', response);
+      } else {
+        console.log("something went wrong with " + ipAddress);
+      }
+    });
+
+};
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
